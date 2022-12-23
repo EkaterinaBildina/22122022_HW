f283141de877a83e63c43b02345ea1e9187269f4
EkaterinaBildina <kukresch@gmail.com>
2022-12-23 11:45:47 +0300

    Task23 Programm.cs write

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, 
+// которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// Пример:
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+
+Console.Write("Введите число N:   ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ThirdPowerTable(number);
+
+void ThirdPowerTable(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine($"{i} | {Math.Pow(i,3)}");
+    } 
+}
+if (number < 1) Console.WriteLine("Проверьте число N (Условие: N > 1)");    
